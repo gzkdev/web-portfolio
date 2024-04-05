81ace28a0be1a926d78af1304710bfbfc89237bb
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-04-05 01:52:59 +0100

    perfect menu animation

--- components/menu.tsx
+++ components/menu.tsx
@@ -1,31 +1,28 @@
 "use client";
 
-import { AnimatePresence, LayoutGroup, motion } from "framer-motion";
 import Link from "next/link";
 import { useState } from "react";
-
-const layoutId = "MENU";
+import { AnimatePresence, motion } from "framer-motion";
 
 export function Menu() {
   const [isOpen, setIsOpen] = useState(false);
   return (
     <div className="fixed bottom-10 left-1/2 -translate-x-1/2 flex items-center justify-center z-10">
-      <LayoutGroup>
+      <AnimatePresence>
         {isOpen ? (
           <motion.div
-            layoutId={layoutId}
-            onClick={() => setIsOpen((x) => !x)}
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0, scale: 0.8 }}
             className="bg-slate-700 text-slate-300 flex flex-col items-center rounded-3xl relative"
           >
-            <button className="absolute top-4 right-4 px-2 py-1 rounded-full border text-sm">
+            <button
+              onClick={() => setIsOpen((x) => !x)}
+              className="absolute top-4 right-4 px-2 py-1 rounded-full border text-sm"
+            >
               Close
             </button>
-            <motion.div
-              initial={{ opacity: 0, scale: 0.8 }}
-              animate={{ opacity: 1, scale: 1 }}
-              exit={{ opacity: 0, scale: 0.8 }}
-              className="w-64 p-4 mt-4"
-            >
+            <motion.div className="w-64 p-4 mt-4">
               <ul className="space-y-2 py-2 px-4 text-center">
                 <li>
                   <Link href="/" className="hover:text-slate-50">
@@ -59,18 +56,18 @@ export function Menu() {
             </motion.div>
           </motion.div>
         ) : (
-          <AnimatePresence>
-            <motion.button
-              layout
-              layoutId={layoutId}
-              onClick={() => setIsOpen((x) => !x)}
-              className="px-4 py-2 bg-slate-700 rounded-full"
-            >
-              <motion.span>Menu</motion.span>
-            </motion.button>
-          </AnimatePresence>
+          <motion.button
+            initial={{ opacity: 0.8, scale: 0.8 }}
+            animate={{ opacity: 0.8, scale: 1 }}
+            exit={{ opacity: 0.8, scale: 0.8 }}
+            whileHover={{ scale: 1.2 }}
+            onClick={() => setIsOpen((x) => !x)}
+            className="px-4 py-2 bg-slate-700 text-slate-300 rounded-3xl inline-flex items-center justify-center"
+          >
+            Menu
+          </motion.button>
         )}
-      </LayoutGroup>
+      </AnimatePresence>
     </div>
   );
 }
