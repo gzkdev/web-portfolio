d17a019d3669873cd58cd69eb149a0eefa2199fe
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-06 14:57:43 +0100

    update navbar

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,28 +1,57 @@
 import Link from "next/link";
 
+const NavLinks = [
+  {
+    id: 0,
+    title: "Projects",
+    href: "/projects",
+  },
+  {
+    id: 1,
+    title: "Demos",
+    href: "/demos",
+  },
+  {
+    id: 2,
+    title: "Articles",
+    href: "/articles",
+  },
+  {
+    id: 3,
+    title: "Resources",
+    href: "/resources",
+  },
+];
+
 export function Navbar() {
   return (
     <nav>
       <div className="max-w-screen-lg mx-auto flex items-center justify-between px-6 py-20">
-        <Link href="/">Ezihe Godswill</Link>
+        <Link href="/" className="font-semibold hover:text-zinc-100 transition">
+          Ezihe Godswill
+        </Link>
 
-        <ul className="hidden sm:flex sm:items-center sm:gap-4 sm:text-sm">
-          <li>
-            <Link href="/articles">Projects</Link>
-          </li>
-          <li>
-            <Link href="/articles">Demos</Link>
-          </li>
-          <li>
-            <Link href="/articles">Articles</Link>
-          </li>
-          <li>
-            <Link href="/articles">Resources</Link>
-          </li>
+        <ul className="hidden sm:flex sm:items-center sm:gap-6 sm:text-sm">
+          {NavLinks.map(({ href, id, title }) => (
+            <li key={id}>
+              <NavbarLink href={href} title={title} />
+            </li>
+          ))}
         </ul>
 
         <button className="size-8 border rounded-full sm:hidden"></button>
       </div>
     </nav>
+  );
+}
+
+function NavbarLink({ title, href }: { title: string; href: string }) {
+  return (
+    <Link
+      href={href}
+      className="relative hover:text-zinc-100 after:content-[''] after:w-full after:h-[1px] after:bg-current after:absolute after:-bottom-0.5 after:left-0 after:scale-x-0 hover:after:scale-x-100 after:transition after:origin-left"
+    >
+      {title}
+    </Link>
   );
 }
