c3be9fb1fa254bfc817e2aed4e717cd012fd5048
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-07 01:52:08 +0100

    update menu

--- components/menu.tsx
+++ components/menu.tsx
@@ -31,7 +31,7 @@ export function Menu() {
               animate="animate"
               exit="initial"
               variants={motionMenuVariant}
-              className="p-2 bg-zinc-800 border border-zinc-700 rounded sm:hidden mr-6 text-sm text-center"
+              className="p-6 bg-zinc-800 border border-zinc-700 rounded-xl sm:hidden mr-6 text-sm text-center shadow-xl"
             >
               {navLinks.map(({ href, id, title }) => (
                 <li key={id}>
@@ -40,10 +40,10 @@ export function Menu() {
                   </Link>
                 </li>
               ))}
-              <li className="outline-none border border-zinc-700 rounded-full bg-zinc-700/50 text-zinc-100 mt-2">
+              <li className="outline-none border border-zinc-700 rounded-full bg-zinc-700/50 text-zinc-100 mt-2 hover:bg-zinc-600/50 hover:border-zinc-600">
                 <Link
                   href="mailto:ezihegodswill01@gmail.com"
-                  className="block py-2 px-6"
+                  className="block py-2 px-10"
                 >
                   Get in touch
                 </Link>
