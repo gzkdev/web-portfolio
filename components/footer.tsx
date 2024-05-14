828b3832530b9183fd18532a452e685ace672263
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-14 12:31:45 +0100

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,30 +1,40 @@
 import { FaXTwitter, FaGithub, FaLinkedin, FaCodepen } from "react-icons/fa6";
 
+const socialMediaLinks = [
+  {
+    name: "Twitter",
+    icon: <FaXTwitter />,
+    url: "https://x.com/gzkdev",
+  },
+  {
+    name: "GitHub",
+    icon: <FaGithub />,
+    url: "https://github.com/gzkdev",
+  },
+  {
+    name: "LinkedIn",
+    icon: <FaLinkedin />,
+    url: "https://linkedin.com/in/godswill-ezihe",
+  },
+  {
+    name: "Codepen",
+    icon: <FaCodepen />,
+    url: "https://codepen.io/gzkdev",
+  },
+];
+
 export function Footer() {
   return (
-    <footer className="px-6">
+    <footer className="px-6 py-24">
       <div className="text-center space-y-6">
-        <ul className="flex items-center justify-center gap-4 flex-wrap text-2xl">
-          <li>
-            <a href="#" aria-label="Twitter" title="Twitter">
-              <FaXTwitter />
-            </a>
-          </li>
-          <li>
-            <a href="#" aria-label="Github" title="Github">
-              <FaGithub />
-            </a>
-          </li>
-          <li>
-            <a href="#" aria-label="LinkedIn" title="LinkedIn">
-              <FaLinkedin />
-            </a>
-          </li>
-          <li>
-            <a href="#" aria-label="Codepen" title="Codepen">
-              <FaCodepen />
-            </a>
-          </li>
+        <ul className="flex items-center justify-center gap-4 flex-wrap text-xl">
+          {socialMediaLinks.map((medium) => (
+            <li key={medium.name}>
+              <a href={medium.url} aria-label="Twitter" title="Twitter">
+                {medium.icon}
+              </a>
+            </li>
+          ))}
         </ul>
         <div className="text-sm">
           <span className="align-middle">&copy;</span> Godswill Ezihe{" "}
