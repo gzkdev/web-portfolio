88cf990e98d42db1e78585379124ef0164af64a3
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-14 12:46:25 +0100

    update projects

--- constants/index.ts
+++ constants/index.ts
@@ -48,40 +48,54 @@ export const navLinks = [
 
 export const projects = [
   {
+    id: "5",
+    name: "Mortar",
+    description:
+      "A light weight blueprint for building decentralized apps with NextJS",
+    type: "Starter kit",
+    tags: ["NextJS", "React", "Web3"],
+    liveUrl: "https://mortar-dusky.vercel.app",
+    url: "https://github.com/gzkdev/mortar",
+  },
+  {
     id: "0",
     name: "Minotes",
     description:
       "A progressive front-end web application built for users to make simple notes, edit them and delete them when they need to.",
-    url: "/",
     type: "Application",
     tags: ["React", "TypeScript"],
+    liveUrl: "",
+    url: "/",
   },
   {
     id: "1",
     name: "Octomigo",
     description:
       "Frontend web application to easilty fetch any Github user's profile without visiting the Github website.users to make simple notes, edit them and delete them when they need to.",
-    url: "/",
     type: "Application",
     tags: ["React", "TypeScript"],
+    liveUrl: "",
+    url: "/",
   },
   {
     id: "2",
     name: "Marche",
     description:
       "An outstanding landing page for a digital marketing agency focused on providing excellent digital services that help fuel the growth of modern businesses.",
-    url: "/",
     type: "Application",
     tags: ["HTML", "CSS", "JavaScript"],
+    liveUrl: "",
+    url: "/",
   },
   {
     id: "3",
     name: "Fiber",
     description:
       "A landing page for an online template marketplace for developers who want a beautiful portfolio template to showcase their projects and apply to jobs.",
-    url: "/",
     type: "Landing page",
     tags: ["HTML", "CSS", "JavaScript"],
+    liveUrl: "",
+    url: "/",
   },
 ];
 
