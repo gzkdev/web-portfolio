3fbc3e418fcb79a9ee7fa09d2a26b48b1ffd51c9
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-16 23:36:12 +0100

    add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="space-y-24 py-24 pb-10 max-w-screen-sm mx-auto">
+      <header className="px-6 space-y-10">
+        <h1 className="text-lg sm:text-2xl">About</h1>
+      </header>
+    </div>
+  );
+}
