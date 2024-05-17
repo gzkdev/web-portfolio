7213ac6288722c9ae7cc4919bbf96872718c7a70
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-17 23:48:11 +0100

    add about page content

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,15 @@
 export default function About() {
   return (
-    <div className="space-y-24 py-24 pb-10 max-w-screen-sm mx-auto">
-      <header className="px-6 space-y-10">
-        <h1 className="text-lg sm:text-2xl">About</h1>
-      </header>
+    <div className="space-y-10 py-24 px-6 pb-10 max-w-screen-sm mx-auto">
+      <div className="size-16 bg-zinc-500 rounded-full"></div>
+      <h2 className="text-lg sm:text-2xl">About</h2>
+      <p>
+        I build scalable, accessible and performant web applications that people
+        love to interact with. Although I`m not a professional designer,
+        sometimes you will find me tinkering on Figma. My love for Front-end
+        engineering lies in learning and building applications people will
+        actually love to use.
+      </p>
     </div>
   );
 }
