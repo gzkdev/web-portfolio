2284cff0ad2d3f3da9cb8ae05c5b631a47d6db50
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-05-22 16:56:48 +0100

    update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,9 +2,13 @@ import { socialMediaLinks } from "@/constants";
 
 export function Footer() {
   return (
-    <footer className="px-6 py-24">
-      <div className="text-center space-y-6">
-        <ul className="flex items-center justify-center gap-4 flex-wrap text-xl">
+    <footer className="w-full max-w-screen-sm px-6 py-20 mx-auto">
+      <div className="w-full flex flex-wrap gap-4 items-center justify-between">
+        <div className="text-sm">
+          <span className="align-middle">&copy;</span> Godswill Ezihe{" "}
+          {new Date().getFullYear()}
+        </div>
+        <ul className="flex items-center justify-center gap-4 flex-wrap">
           {socialMediaLinks.map((medium) => (
             <li key={medium.name} className="hover:text-white transition">
               <a href={medium.url} aria-label="Twitter" title="Twitter">
@@ -13,10 +17,6 @@ export function Footer() {
             </li>
           ))}
         </ul>
-        <div className="text-sm">
-          <span className="align-middle">&copy;</span> Godswill Ezihe{" "}
-          {new Date().getFullYear()}
-        </div>
       </div>
     </footer>
   );
